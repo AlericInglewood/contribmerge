dce0a98eacb8bec395e257cba81cb7cf0d0994cc
Aleric Inglewood <Aleric.Inglewood@gmail.com>
2011-01-19 22:56:39 +0100

    Added lot of comments

--- src/example/example.cc
+++ src/example/example.cc
@@ -115,30 +115,54 @@ namespace grammar
       using qi::string;
       using ascii::char_;
 
+      // The first name of a contributor.
+      // Attribute: std::string.
       contributor_first_name =
 	  alpha >> *alnum
       ;
 
+      // Whitespace plus last name of a contributor (optionally follows contributor_first_name).
+      // Attribute: std::string.
       contributor_last_name =
-	  &(+blank >> alpha) >> omit[+blank] >> qi::attr(' ') >> +alpha
-	  //-(omit[+blank] >> qi::attr(' ') >> +alpha)
+	  // The &(+blank >> alpha) stops this rule from adding a trailing space
+	  // to the attribute, only to find out that it fails because there is
+	  // no last name (the attribute is not reset in that case).
+	  &(+blank >> alpha) >>
+	  // At least one blank (translated into a single space), followed by alpha characters.
+	  omit[+blank] >> qi::attr(' ') >> +alpha
       ;
 
+      // The full name of a contributor. If there is no last name, then the assumption has
+      // to be that it is "FirstName Resident".
+      // Attribute: std::string.
       contributor_full_name =
+	  // The trick with eps is to simulate 'optional'. Using a real
+	  // optional doesn't work here because of a bug in boost::spirit
+	  // (it would cause the attribute to be reset).
+	  // This should be fixed in boost >= 1.46.
 	  contributor_first_name >> (contributor_last_name | qi::eps)
-	  // The raw[] is needed for boost < 1.46.
-	  // Unfortunately this causes blanks between first and last to be returned literally.
-	  //contributor_first_name >> raw[contributor_last_name];
       ;
 
-      empty_line =
+      // An eol of sequence including optional preceding white space.
+      // Trailing white space is always condensed into a single eol on output, hence it has no attribute.
+      newline =
 	  *blank >> eol
       ;
 
-      start =
-	  empty_line >> contributor_full_name >> *blank >> eol
+      // A line existing of (optionally) only blanks, including the eol.
+      // Empty lines are always condensed into a single eol on output, hence it has no attribute.
+      empty_line =
+	  newline
       ;
 
+      // Helper rule to find the start of the contributors list.
+      // This rule has no attribute.
+      start =
+	  empty_line >> contributor_full_name >> newline;
+      ;
+
+      // A jira project key prefix.
+      // Attribute: std::string.
       jira_project_key_prefix =
 	  (
 		string("VWR")  // 1. Second Life Viewer - VWR
@@ -154,41 +178,54 @@ namespace grammar
 	  )
       ;
 
+      // A jira project key.
+      // The attribute is JiraProjectKey.
       jira_project_key =
 	    ("[NO JIRA]" >> qi::attr(0))
 	  | (jira_project_key_prefix[&print_test] >> '-' >> int_[&print_int])
       ;
 
+      // Any character except eol characters.
+      // Attribute: char.
       any_char_but_eol =
 	  char_ - '\r' - '\n'
       ;
 
+      // A space trimmed comment.
+      // Attribute: std::string.
       comment =
-	  *(!(*blank >> eol) >> char_)
+	  // The !newline causes trailing white space not to be included,
+	  // and to terminate at the end of a line.
+	  *(!newline >> char_)
       ;
 
-      optional_comment =
-	    (omit[+blank] >> comment >> omit[*blank])
-	  | omit[*blank]
-      ;
-
+      // Mandatory blank(s) followed by a jira project key, followed by a comment (that may be empty).
+      // Attribute: ContributionEntry.
       contribution_entry =
-	  omit[+blank] >> jira_project_key >> optional_comment >> eol
+	  omit[+blank] >> jira_project_key >> comment >> newline;
       ;
 
+      // Name of a contributor, followed by a list of their contributions.
+      // Attribute: Contributor.
       contributor =
-	  contributor_full_name >> omit[*blank] >> eol >> *contribution_entry
+	  contributor_full_name >> newline >> *contribution_entry
       ;
 
+      // As an exception, the header line preserves trailing whitespace.
+      // Attribute: std::string.
       header_line =
 	  *any_char_but_eol >> eol
       ;
 
+      // The complete header, exclusive the final empty line.
+      // This is a literal string containing newlines and trailing whitespaces.
+      // Attribute: std::string.
       header =
-	  // Return the header literally.
+	  // Return the header literally. Stop when we see 'start'.
 	  raw[*(header_line - start)]
       ;
 
+      // Attribute: ContributionsTxt.
       contributions_txt =
 	  header >> empty_line >> +contributor >> *empty_line
       ;
@@ -197,6 +234,7 @@ namespace grammar
     qi::rule<Iterator, std::string()> contributor_first_name;
     qi::rule<Iterator, std::string()> contributor_last_name;
     qi::rule<Iterator, std::string()> contributor_full_name;
+    qi::rule<Iterator> newline;
     qi::rule<Iterator> empty_line;
     qi::rule<Iterator> start;
     qi::rule<Iterator, char()> any_char_but_eol;
@@ -205,7 +243,6 @@ namespace grammar
     qi::rule<Iterator, std::string()> jira_project_key_prefix;
     qi::rule<Iterator, JiraProjectKey()> jira_project_key;
     qi::rule<Iterator, std::string()> comment;
-    qi::rule<Iterator, std::string()> optional_comment;
     qi::rule<Iterator, ContributionEntry()> contribution_entry;
     qi::rule<Iterator, Contributor()> contributor;
     qi::rule<Iterator, ContributionsTxt()> contributions_txt;
@@ -228,10 +265,10 @@ std::string const input =
 
 "JustFirstname\n"
   "\tVWR-201\n"
-  "\t[NO JIRA] (but worked hard on this)\n"
+  "\t[NO JIRA]\t\t(but worked hard on this)\n"
   "\tSTORM-203  \n"
 
-"JustFirstnameWith3Space   \n"
+"JustFirstnameWith3Spaces   \n"
    " STORM-301  \n"
 
 // Trailing white spaces.
